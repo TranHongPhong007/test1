f717f68fbe0fec920d1a403d05654c9322a7d1b0
LENOVO <LENOVO@DESKTOP-6AD99C2>
2025-03-25 23:42:55 +0700

    second commit

---
+++ src/java/model/Student.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Student {
+    private String studentID;
+    private String name;
+    private String dob;
+    private String tel;
+
+    public Student() {
+    }
+
+    public Student(String studentID, String name, String dob, String tel) {
+        this.studentID = studentID;
+        this.name = name;
+        this.dob = dob;
+        this.tel = tel;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    
+    
+}
